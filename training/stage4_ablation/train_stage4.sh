bbb6c33deff81b002b0e5b948abd4e92c0d4f5b6
Anonymous <anonymous@acl2026.org>
2026-01-05 19:54:18 +0700

    Remove anonymous

--- training/stage4_ablation/train_stage4.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run training
-python train.py --config config_stage4.yaml
-
-# Optional: If you want to override the device in config
-# python train.py --config config_stage4.yaml --device cuda:0
